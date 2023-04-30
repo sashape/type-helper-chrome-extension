c0fa39063531ef0f08c966133cf391dce7a9132b
sashape <apien@list.ru>
2023-04-30 23:58:18 +0300

    feat: add popup page with logged screen

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,212 @@
 <template>
-  <div class="">Hello</div>
+  <popup/>
 </template>
 
 <script>
 
+import Popup from "@/Popup.vue";
+
 export default {
   name: 'App',
   components: {
+      Popup
 
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+body {
+  background-color: #f7f7f8;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
+  font-size: 14px;
+  line-height: 1.4;
+  color: #0f0f0f;
+  * {
+    box-sizing: border-box;
+  }
 }
+
+.container {
+  position: relative;
+  width: 400px;
+  margin: 50px auto;
+  padding: 20px;
+  background-color: #fff;
+  border-radius: 4px;
+  box-shadow: 0 10px 30px rgba(0, 0, 0, 0.1);
+  text-align: center;
+
+  .logo {
+    margin-bottom: 30px;
+
+    img {
+      width: 80px;
+      height: 80px;
+      object-fit: contain;
+    }
+  }
+
+  .title {
+    font-size: 24px;
+    font-weight: 600;
+    margin-bottom: 10px;
+  }
+
+  .form-control {
+    display: block;
+    width: 100%;
+    padding: 10px;
+    border: 1px solid #dcdfe6;
+    border-radius: 4px;
+    margin-bottom: 15px;
+    font-size: 14px;
+    line-height: 1.4;
+
+    &:focus {
+      outline: none;
+      border-color: #4d90fe;
+      box-shadow: 0 0 0 3px rgba(77, 144, 254, 0.1);
+    }
+
+    &::-webkit-input-placeholder {
+      color: #bbb;
+    }
+
+    &:-ms-input-placeholder {
+      color: #bbb;
+    }
+
+    &::-ms-input-placeholder {
+      color: #bbb;
+    }
+
+    &::placeholder {
+      color: #bbb;
+    }
+
+    &-label {
+      display: block;
+      font-size: 12px;
+      font-weight: 600;
+      text-align: left;
+      margin-bottom: 5px;
+    }
+  }
+
+  .btn {
+    display: inline-block;
+    padding: 12px 24px;
+    font-size: 16px;
+    font-weight: 600;
+    text-align: center;
+    white-space: nowrap;
+    vertical-align: middle;
+    cursor: pointer;
+    border-radius: 4px;
+    background-color: #4d90fe;
+    color: #fff;
+    border: none;
+    transition: all 0.3s ease;
+    margin-bottom: 10px;
+    &:hover {
+      background-color: #4787ed;
+    }
+
+    &:active {
+      transform: translateY(1px);
+    }
+
+    &:focus {
+      outline: none;
+    }
+
+    i {
+      margin-right: 5px;
+    }
+    &.outline {
+      background-color: #fff;
+      color: #0f0f0f;
+      border: 1px solid #4d90fe;
+    }
+  }
+
+  .btn-google {
+    background-color: #fff;
+    border: 1px solid #dcdfe6;
+    color: #606469;
+
+    &:hover {
+      background-color: #fafbfc;
+    }
+
+    i {
+      color: #fbbc05;
+    }
+  }
+
+  .btn-facebook {
+    background-color: #3b5998;
+
+    &:hover {
+      background-color: #2d4373;
+    }
+
+    i {
+      color: #fff;
+    }
+  }
+  .create-account {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    color: #4d90fe;
+    font-weight: bold;
+    text-decoration: none;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+}
+.form-group {
+  margin-bottom: 15px;
+}
+.avatar {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  margin: 0 auto;
+}
+
+.user-info {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.name {
+  margin: 0;
+  font-size: 18px;
+  font-weight: 700;
+}
+
+.plan {
+  margin: 5px 0 0;
+  font-size: 14px;
+  font-weight: 600;
+  color: #9b9b9b;
+}
+
+.content {
+  padding: 20px;
+  text-align: center;
+}
+
+.content p {
+  margin: 0 0 20px;
+  font-size: 16px;
+  font-weight: 600;
+}
+
 </style>
--- src/Popup.vue
+++ src/Popup.vue
@@ -1,27 +1,90 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="logo"><img src="assets/icons/icon128.png" alt="Logo">
+    </div>
+    <div
+        v-if="!isLogged"
+    >
+      <a href="#" class="create-account">Create account</a>
+
+      <div class="form-group">
+        <div class="title">Log in to your account</div>
+        <button
+            @click="openTab('login')"
+            type="submit" id="submit" class="btn btn-primary"><i class="fas fa-sign-in-alt"></i>Log in</button>
+      </div>
+      <div class="form-group">
+        <div class="title">Or sigh up</div>
+        <button
+            @click="openTab('register')"
+            type="submit" id="submit" class="btn btn-primary outline"><i class="fas fa-sign-in-alt"></i>Sigh Up</button>
+      </div>
+    </div>
+    <div v-else>
+      <div class="header">
+        <img class="avatar" src="assets/images/male-avatar.jpg" alt="User Avatar">
+        <div class="user-info">
+          <h3 class="name">John Doe</h3>
+          <p class="plan">Starter Plan</p>
+        </div>
+      </div>
+      <div class="content">
+        <p>You can now access all the features of our Starter Plan.</p>
+        <button
+            @click="openTab('account')"
+            type="submit" id="submit" class="btn btn-primary outline"><i class="fas fa-sign-in-alt"></i>Account</button>
+      </div>
+    </div>
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
+// import CryptoJS from 'crypto-js';
 export default {
-    // eslint-disable-next-line vue/multi-word-component-names
     name: 'Popup',
     components: {
-        HelloWorld
+    },
+    data: () => ({
+        email: '',
+        pass: '',
+        encryptString: ''
+    }),
+    methods: {
+        openTab(url) {
+            // const queryParams = this.formatQueryParams({ email: this.email, password: this.pass });
+            // const queryParamsEncrypt = this.formatQueryParams({ request: CryptoJS.AES.encrypt(queryParams, 'secret')});
+            // const url = `https://example.com/login?${this.formatQueryParams(queryParamsEncrypt)}`;
+            window.open(`https://example.com/${url}`, '_blank');
+        },
+        // formatQueryParams(params) {
+        //     return Object.keys(params)
+        //         .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+        //         .join('&');
+        // },
+        openSighUpTab() {
+            window.open('https://example.com/sighup', '_blank');
+        },
+        getCookie(name) {
+            const cookies = document.cookie.split(';');
+            for (let i = 0; i < cookies.length; i++) {
+                const cookie = cookies[i].trim();
+                if (cookie.startsWith(`${name}=`)) {
+                    return cookie.substring(name.length + 1, cookie.length);
+                }
+            }
+            return null;
+        }
+    },
+    computed: {
+      isLogged() {
+          return this.getCookie('TH_islogged');
+      }
+    },
+    created() {
+        document.cookie = "TH_islogged=true; expires=Sat, 30 Apr 2045 23:59:59 GMT; path=/";
+        console.log(this.getCookie('TH_islogged'))
     }
+
 }
 </script>
-
-<style>
-#app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-}
-</style>
